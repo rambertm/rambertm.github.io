9ac55a91ced1461f3d5dda3bbbc3a3c518e7ec2c
rambertm <58461570+rambertm@users.noreply.github.com>
2021-01-22 16:52:46 +0900

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,1 +1,8 @@
+let no = 1;
 
+Document.getElementById('addDoctor').addEventListener('click', addDoctor);
+
+function addDoctor(e){
+  Document.getElementById('doctorList').insertAdjacentHTML('beforeend', 'Doctor' + no);
+  no++
+}
