7437531edd0cad136465ab2d3dea5db90f0465aa
rambertm <58461570+rambertm@users.noreply.github.com>
2021-01-27 21:02:09 +0900

    Update polyfill.js

--- polyfill.js
+++ polyfill.js
@@ -215,3 +215,23 @@ if (!Number.MAX_SAFE_INTEGER) {
 Number.isSafeInteger = Number.isSafeInteger || function (value) {
    return Number.isInteger(value) && Math.abs(value) <= Number.MAX_SAFE_INTEGER;
 };
+
+// Overwrites native 'firstElementChild' prototype.
+// Adds Document & DocumentFragment support for IE9 & Safari.
+;(function(constructor) {
+    if (constructor &&
+        constructor.prototype &&
+        constructor.prototype.firstElementChild == null) {
+        Object.defineProperty(constructor.prototype, 'firstElementChild', {
+            get: function() {
+                var node, nodes = this.childNodes, i = 0;
+                while (node = nodes[i++]) {
+                    if (node.nodeType === 1) {
+                        return node;
+                    }
+                }
+                return null;
+            }
+        });
+    }
+})(window.Node || window.Element);
