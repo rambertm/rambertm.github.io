a0dfca046c2c327295c3e1ed427a2adc4a5fd23e
rambertm <58461570+rambertm@users.noreply.github.com>
2021-01-28 09:44:33 +0900

    Update polyfill.js

--- polyfill.js
+++ polyfill.js
@@ -256,3 +256,18 @@ Number.isSafeInteger = Number.isSafeInteger || function (value) {
   }
 })(window.Node || window.Element);
 
+/*
+* String.prototype.includes()  POLYFILL
+**/
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+
+    if (search instanceof RegExp) {
+      throw TypeError('first argument must not be a RegExp');
+    }
+    if (start === undefined) { start = 0; }
+    return this.indexOf(search, start) !== -1;
+  };
+}
+//-------------------------------------//
